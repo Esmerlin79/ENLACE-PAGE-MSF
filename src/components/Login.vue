cfde9bf4ae3d667de35806494fd20eef17983e54
Esmerlin79 <68618650+Esmerlin79@users.noreply.github.com>
2020-11-04 11:37:40 -0400

    COMMIT LOGIN
    Este es un commit

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+        <div class="main-sign">
+           
+            <div class="container-input">
+                <div class="logo">
+                    <img src="img/logo.png" alt="Logo Pic">
+                </div>
+
+                <div class="sign-inp">
+                    <p class="clr-darkBlue fnt-medium">Correo Electronico</p>
+                    <input type="text" class="input-field">
+                </div>
+                <div class="sign-inp">
+                    <p class="clr-darkBlue fnt-medium">Contraseña</p>
+                    <input type="password" class="input-field">
+                </div>
+      
+                <div class="recovery-singn">
+            <p class="clr-gray-a"><a href="#">Olvidaste vuestra Contraseña?</a></p>
+            <p class="clr-gray">Todavía no tienes tu cuenta? <a href="/register" class="reg-sign">Registrate</a></p>
+                </div> 
+                   
+                <a href="#"  class="btn btn-blue">Ingresar</a>
+            </div>   
+
+            <div class="sign-pic">
+                <img src="img/gov.jpg" alt="gov image">
+            </div>
+        </div>
+</template>
